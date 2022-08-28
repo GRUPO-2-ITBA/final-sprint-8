e416c00539da0d5303b02e7af91fc720ad4037bd
Julian <105244530+Julianfried@users.noreply.github.com>
2022-08-28 20:51:27 -0300

    serializer.py

--- homebanking/cuentas/serializers.py
+++ homebanking/cuentas/serializers.py
@@ -1,1 +1,12 @@
 
+from rest_framework import serializers
+from .models import Cuenta, TipoCuenta
+from django.db import models
+
+
+class CuentasSerializer(serializers.ModelSerializer):
+    account_type = serializers.IntegerField(source="account_type_id")
+
+    class Meta:
+        model = Cuenta
+        fields = ("balance", "iban", "account_type")
