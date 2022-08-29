3eab20acf501d46e3952917f24bc9b25e5470c22
fllorgarcia <77365971+fllorgarcia@users.noreply.github.com>
2022-08-28 22:50:28 -0300

    Update serializers.py

--- homebanking/api/serializers.py
+++ homebanking/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+
 from .models import Sucursal
+
 from .models import Movimientos
+
 from .models import Direcciones
+
 
 
 class SucursalesSerializer(serializers.ModelSerializer):
@@ -9,20 +13,22 @@ class SucursalesSerializer(serializers.ModelSerializer):
         model = Sucursal
         # indicamos que use todos los campos
         fields = "__all__"
-        # les decimos cuales son los de solo lectura
+        # les decimos cuales son  de solo lectura
 
 
+        
 class MovimientosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movimientos
         # indicamos que use todos los campos
         fields = "__all__"
-        # les decimos cuales son los de solo lectura
+        # les decimos cuales son  de solo lectura
 
+        
 
 class DireccionesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Direcciones
         # indicamos que use todos los campos
         fields = "__all__"
-        # les decimos cuales son los de solo lectura
+        # les decimos cuales son  de solo lectura
